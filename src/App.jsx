7f78418a613d64d9ab5988936ac7263e657cf094
kashi-verma <kashishverma2124@gmail.com>
2025-07-06 13:22:25 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Components/LandingPage";
-import SignInPage from "./Components/SignInPage";
 import SignUpPage from "./Components/SignUpPage";
+import SignInPage from "./Components/SIgnInPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage/>} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
