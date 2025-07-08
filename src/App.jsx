f162d90fee291930390c0b42716d8ededa42f0fc
kashi-verma <kashishverma2124@gmail.com>
2025-07-08 23:32:31 +0530

    added Homepage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Components/LandingPage";
 import SignUpPage from "./Components/SignUpPage";
-import SignInPage from "./Components/SIgnInPage";
+import SignInPage from "./Components/SIgnInPage";// Corrected typo: SIgnInPage -> SignInPage
+import HomePage from "./Components/HomePage";     // Import your new HomePage
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<HomePage />} /> {/* New HomePage route */}
       </Routes>
     </Router>
   );
